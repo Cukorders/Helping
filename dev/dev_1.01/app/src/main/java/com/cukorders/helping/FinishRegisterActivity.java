577c4484484550febde9da55aaef8511074cccba
LeeSoMyoung <so4644009@gmail.com>
2020-05-14 00:08:42 +0900

    google map 적용 이전
    regional_certification1 controller 일부 기능 구현

---
+++ dev/dev_1.01/app/src/main/java/com/cukorders/helping/FinishRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.cukorders.helping;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FinishRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finish_newprofile);
+
+    }
+}
