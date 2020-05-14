af63845791db34e20713e1a0b44d877419b4bae3
LeeSoMyoung <so4644009@gmail.com>
2020-05-14 13:23:47 +0900

    중간 수정

--- dev/dev_1.01/app/src/main/java/com/cukorders/helping/RegisterFinishActivity.java
+++ dev/dev_1.01/app/src/main/java/com/cukorders/helping/RegisterFinishActivity.java
@@ -12,16 +12,11 @@ public class RegisterFinishActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.regional_certification2);
+        setContentView(R.layout.finish_newprofile);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* TODO
-                유저 정보를 받아왔을 시 => Main으로 이동(아래 코드)
-                유저 정보를 받아오지 못했을 시 => 로그인 및 회원가입 화면으로 전환
-                **/
-
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
                 finish();
