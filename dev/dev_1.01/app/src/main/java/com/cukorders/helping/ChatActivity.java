7af86db6c16071ed9726cbca2e19f6ca91258a0b
parkss <sangsu9701@naver.com>
2020-05-23 20:30:27 +0900

    chatting(수정2)

--- dev/dev_1.01/app/src/main/java/com/cukorders/helping/ChatActivity.java
+++ dev/dev_1.01/app/src/main/java/com/cukorders/helping/ChatActivity.java
@@ -16,6 +16,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
 
     private String CHAT_NAME;
